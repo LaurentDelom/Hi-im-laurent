444d70ea54e316cd48d6ff8b5841641e2987d972
Laurent Delom <laurentmezerac@hotmail.com>
2023-02-22 22:38:31 +0100

    "vh responsive" to test

--- request.js
+++ request.js
@@ -573,5 +573,16 @@ function updateScroll(){
     //console.log("Scroll updated");
 }
 
+//////////////// RESIZE MANAGER for VH on mobile ////////////
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
