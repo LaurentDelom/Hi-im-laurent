f82aff2d4ac4a037c07e740947ce1349cb5a744d
Laurent Delom <laurentmezerac@hotmail.com>
2023-02-24 14:57:14 +0100

    update scroll after resize event

--- request.js
+++ request.js
@@ -596,5 +596,35 @@ function updateScroll(){
 
 
 
+/////////////// RESIZE MANAGER for VH on mobile ////////////
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.visualViewport.height * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+/*
+const chatForm = document.querySelector('input[type="text"]'); 
+
+chatForm.addEventListener("focus",(event) => {
+    document.querySelector(".chatbox-user").style.top="40vh";
+    document.getElementById("conversation-thread").style.height = "38vh";
+    updateScroll();
+});
+
+chatForm.addEventListener("blur",(event) => {
+    document.querySelector(".chatbox-user").style.top="90vh";
+    document.getElementById("conversation-thread").style.height = "88vh";
+    updateScroll();
+});
+*/
+
+if (window.visualViewport) {
+    window.visualViewport.addEventListener("resize", () => {
+        vh = window.visualViewport.height * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        dateNextScroll = Date.now() + 250; 
+        nextScrollToDo = true;
+    });
+  }
 
