7adfb9686262303c02dbcde831b325c4dee3fc7f
Laurent Delom <laurentmezerac@hotmail.com>
2023-03-09 17:07:39 +0100

    "update of topics-sheet"

--- topics-sheet.js
+++ topics-sheet.js
@@ -2,11 +2,268 @@ export const topics = [
     {   
         "key":"intro",
         "images":[],
-        "texts":["So nice to see you here!","Let me show you how it works:","Just enter one of the following keywords: <b>work</b>, <i>nature</i>, technique, about myself, internet...","There you are! Ready to go!","Where would you like to start?"],
+        "texts":["So nice to see you here!","This is the platform where I speak about what I do, in the shape of this <b>chatbox</b>. Welcome!","As we go, you will see some keywords appear in <b>bold</b>. By sending these words in the chat, you are the one to navigate. I respond with the matching information.","Would you like to start with an overview of my <b>existing works</b>? or my <b>themes</b> of reflection? And later move on to my <b>work forecast</b>? ","Or ask me about my <b>latest news</b>, or <b>about myself</b>!","Where shall we start?"],
         "related":[],
-        "waiting":[200,200,400,300,300], //[3200,4200,4000,3000,3000],
+        "waiting":[200,200,400,300,300,300], //[3200,4200,4000,3000,3000],
         "transverse":false
     },
+
+    {   
+        "key":"bold",
+        "images":[],
+        "texts":["Correct!","Bold is indeed the firsi keyword...","You’re so meta!","*first"],
+        "related":["existing","forecast","themes","news","myself"],
+        "waiting":[200,200,400,150], 
+        "transverse":false
+    },
+
+    {   
+        "key":"existing",
+        "images":[],
+        "texts":["A good part of my existing works were made during a research called <b>.ext</b> between 2019 and 2022.","Like <b>Silicon Mountain</b>, <b>BULK</b>, <b>intuit®</b>, or the <b>Stone Beauty Contest</b>.","Before that I co-created a dance duet called <b>duet</b> with my colleague and friend Dan Mussett. And a first solo piece <b>if any question</b>.","In parallel I like to <b>design</b> my artist page as mini-performances on <b>internet</b>: the <b>2016</b> edition and this current <b>chatbox</b>."],
+        "related":["ext","silicon","bulk","contest","intuit","chatbox","2016","internet","design","duet","question"],
+        "waiting":[200,200,400,300], 
+        "transverse":false
+    },
+
+    {   
+        "key":"ext",
+        "images":[],
+        "texts":[".ext was a 3 year long research between October 2019 and June 2022.", 
+            "During which I produced several works.",
+            "At first an exploration of the figure of the mountaineer. The starting point was to situate this practice in its specific socio-cultural context (white, educated, masculine, privileged) and contingency to our western history.",
+            "A legacy of unspoken rules pre-scripts the whole practice, imposing re-enactment as default behavior. It structures our desires, our perceptions, our subjective experience and our ethos when we are in nature.",
+            "My work hypothesis was : what we call nature is a dedicated space where we perform a fixed idea of Nature.",
+            "To <i>speculate</i> was one of the <b>action verbs</b> that drove the research. For example <b>Silicon Mountain</b> speculates on the contemplation of nature under the pressure of technology, <b>BULK</b> on the commodification of hiking, <b>intuit</b> on our faith in Gore-tex, and <b>Stone Beauty contest</b> on the origins of beauty in nature.",
+            "Some <b>themes</b> will continue to pervade my <b>work forecast</b> such as <b>nature</b>, <b>technology</b>, <b>internet</b>. As well as an emerging <b>research methodology</b>."],
+        "related":["existing","forecast","themes","nature","technology","methodology","verbs","silicon","bulk","inuit","contest"],
+        "waiting":[200,1000,200,400,150,300,200], 
+        "transverse":false
+    },
+
+    {   
+        "key":"silicon",
+        "images":[],
+        "texts":["Silicon Mountain is the main outcome of the <b>.ext</b> research. It is a performance that speculates on what contemplation of nature means today.", 
+            "It opens with descriptions of sublime landscapes, taken from XIXth century literature. Then we dive into the mind of a genius Silicon Valley entrepreneur launching his Contemplation-of-Nature App. With this downloaded, our longing for beauty in nature will be quenched once and for good. Then the audience gets access to the App.", 
+            "Tadaaa!",
+            " And it really works!",
+            "Different layers of questioning. What do we look at when we look at nature? How do we deal with technology today? (mediatizing large parts of our interaction with the world: social life, entertainment, memory...)  Will it go as far as to cover our connection with nature?", 
+            "And WTF is an ‘im-mediate connection’ with nature anyways?",
+            "Voilà. OK. I stop. I got carried away. It was the biggest chunk in the <b>chatbox</b>, I promise.",
+            "Oh almost forgot >>> </br> the <b>themes</b> are <b>nature</b>, <b>technology</b>, <b>internet</b>, <b>technique</b>, <b>design</b>, <b>entrepreneur</b>. And <b>methodology</b>"
+        ],
+            
+        "related":["existing","ext","nature","technology","methodology","verbs","technique","internet","design","entrepreneur"],
+        "waiting":[200,1000,200,200,400,150,300,200], 
+        "transverse":false
+    },
+
+    {   
+        "key":"bulk",
+        "images":[],
+        "texts":["BULK is a fictional entrepreneurial project that brings the first indoor hiking gym to the world.", 
+            "It takes the form of a commercial facility branded BULK. Just like climbing gyms, skating-rinks, indoor skiing, indoor skydiving, indoor swimming… Now we have a place to practice indoor hiking!", 
+            "Set the level of difficulty and our A.I software INFINITRACK video-maps the route for you! Share your stats with your friends and give each other kudos!", 
+            "<b>#entrepreneur</b>!!!",
+            "Speculating on this idea, BULK was co-designed with architect Jules Flocke and graphic-designer Timon Bloemen. ",
+            "<b>#design</b>!!", 
+            "Amongst my <b>work forecast</b>, there is this fantasy about going further with <b>BULK2</b>.", 
+            "In any case, it’s a very very good illustration of my <b>action verbs</b> and thoughts on <b>nature</b>",
+        ],
+            
+        "related":["existing","ext","nature","technology","verbs","design","entrepreneur","bulk2"],
+        "waiting":[200,1000,200,200,400,150,300,200], 
+        "transverse":false
+    },
+
+    {   
+        "key":"intuit",
+        "images":[],
+        "texts":["Intuit® is pronounced “into-it” but derives its spelling from “intuition”. It is a textile work co-designed with (and entirely realized by) Ruby Russell.", 
+            "It is inspired by how Amazonian indigenous tribes devised special garments to invoke protection when roaming the forest. Think about the bright red and blue feather headdresses, or jaguar teeth necklaces...", 
+            "Adapted to our Western practice of the great outdoors, intuit® is made of technical water-proof Gore-Tex® fabric. The protection is woven within the technological fibers of the fabric, protecting you from the elements. It is not visible but it works just like magic." ,
+            "Intuit® is meant to be worn on the left arm and/or to be on sale in a shop window and/or exposed in an ethnographic museum. ", 
+            "Only so much for Intuit®… It is linked to <b>nature</b>, <b>technology</b>, in the <b>.ext</b> research."
+        ],
+            
+        "related":["existing","ext","nature","technology","verbs","design"],
+        "waiting":[200,1000,200,200,400], 
+        "transverse":false
+    },
+
+    {   
+        "key":"contest",
+        "images":[],
+        "texts":[
+            "Oh I loved that one!", 
+            "The Stone Beauty Contest was a one-time online event organized with the Wandering Arts Biennale (Brussels) in December 2020.", 
+            "Participants were asked to submit the most beautiful stone they had ever picked. They had to write about why their stone should win. We could ask questions and challenge each other’s assumptions on true authentic stone beauty.", 
+            "Come to think of it, it’s quite cute and absurd to pick up stones as we hike... Do we find beauty in stones? Or are stones beautiful in themselves?", 
+            "A legal vote designated the winner, earning it a shrine in the WAB office in Brussels.",
+            "The Stone Beauty Contest is a franchise that could be reproduced, but still hasn’t."
+        ],
+            
+        "related":["existing","ext","nature","verbs"],
+        "waiting":[200,150,1000,200,200,400], 
+        "transverse":false
+    },    
+
+    {   
+        "key":"duet",
+        "images":[],
+        "texts":[
+            "<i>duet</i> is a dance piece actually entitled <i>dddddddduettttttttttt</i>.", 
+            "It was co-created with Dan Mussett, a friend from the Conservatoire of Antwerp. We wanted to learn <i>how to become each other.</i>", 
+            "The motif of modifying who you are was strongly highlighted by our trajectories at school and by what started to boom in those same years: a whole new body culture of Youtube tutorials. Yoga, fitness, calisthenics… <br> you-friggin-name-it!", 
+            "Caught in that injunction to learn and transform, we assessed how it could be adapted to our goal of becoming each other.",
+            "The piece reveals the dynamics between learning and teaching, and repeating repeating repeating…", 
+            "So yeah, the theme of <b>internet</b> was already there back then. The <b>action verbs</b> too, somehow. And the first seeds of a <b>research methodology</b>.",
+            "Oh and do check Dan’s project: The Dating Project (on Instagram)."
+        ],
+            
+        "related":["existing","internet","verbs","methodology"],
+        "waiting":[200,150,1000,200,200,200,400], 
+        "transverse":false
+    },
+
+    {   
+        "key":"question",
+        "images":[],
+        "texts":[
+            "<i>IF ANY QUESTION</i> is a solo performance I made in my 2nd year at school.", 
+            "It’s an intimate performance about love and disillusion, where the main protagonist is a toaster. He gives the timing and all the cues. At the end, the last words is his.",
+            "As I read the opening chapter of a recipe book, contrasting feelings and confusion arise in the audience. We are talking about family here.", 
+            "Five minutes long. Three people at a time.",
+            "When I look back at it, I see: <b>choreographing technique</b>, and a first pass at some of my <b>action verbs.</b>",
+            "<3"
+        ],
+            
+        "related":["existing","technique","verbs"],
+        "waiting":[200,150,1000,200,200,1000], 
+        "transverse":false
+    },
+
+    {   
+        "key":"2016",
+        "images":[],
+        "texts":[
+            "In 2016 I designed my first artist’s webpage as a mini-performance to present my aspirations as a young artist.", 
+            "It was named <i>About the movie</i>, looking like rolling credits at the end of films. It follows a dramaturgy of screenshots, and navigates linearly through a fake cult film from the 1960’s.",
+            "It’s really cool actually. You can look it up at laurentdelom.com/2016 <br> (although the contents are totally outdated by now)",
+            "That website was my first artistic object on <b>internet</b> after graduating. It has been replaced in 2023 by this <b>chatbox</b> entitled “Hi! I’m Laurent”: you are on it right now." 
+            
+        ],            
+        "related":["existing","technique","verbs","internet","design","chatbox"],
+        "waiting":[200,150,1000,200,200,1000], 
+        "transverse":false
+    },
+
+    {   
+        "key":"chatbox",
+        "images":[],
+        "texts":[
+            "For the wrap-up of <b>.ext</b> I organized 2 <i>Easy Conversations</i> taking place on WhatsApp to present the overall research.", 
+            "The thing was: there was just too much to say to be exhaustive.", 
+            "So the principle was the same as here",
+            "I let the audience make the cut and select what they wanted to read about.", 
+            "and I just make the structure for them to navigate through.",
+            "A bit like a neural network, where we follow the most triggering interest from one node to another like a nerve impulse.",
+            "Huhu",
+            "Of course, I am also fascinated by how familiar we have become with written-oral conversations on the <b>internet</b> and the specific textuality that it produces.", 
+            "I mean:", 
+            "hellllooooo!",
+            "+ I like that it is playful & colorful & representative of my practice around <b>design</b> and <b>entrepreneur</b> and with my <b>action verbs</b>...",
+            
+            
+        ],            
+        "related":["existing","technique","verbs","internet","design","2016","ext"],
+        "waiting":[200,150,1000,200,200,1000,100,100,100,100,100], 
+        "transverse":false
+    },
+
+    {   
+        "key":"forecast",
+        "images":[],
+        "texts":[
+            "<i>Work forecast?</i>",
+            "What does that even mean?",
+            "I just call this all the potential things that I <i>might</i> want to make in the future.",
+            "Some fresh ideas, many vague visions, too much thinking.",
+            "But listen, here is what <i>might</i> happen :", 
+            "<b>Travail!</b>, a now mature research project going in production, as we speak.", 
+            "<b>deep beliefs</b> my next “big research” after <b>.ext</b>", 
+            "Expect water, darkness and prayers that it all goes well.",
+            "maybe maybe <b>Vanilla</b> a kind of social network for long distance sexual arousal.", 
+            "And going further with <b>BULK2</b>."
+                        
+        ],            
+        "related":["travail","deep","vanilla","BULK2","ext"],
+        "waiting":[200,150,1000,200,200,1000,100,100,100,100], 
+        "transverse":false
+    },
+
+    {   
+        "key":"travail",
+        "images":[],
+        "texts":[
+            "<i>Travail!</i> is a project co-created with Mina Perrichon, and supported by the program Chimères, in France.",
+            "It will become a performance flirting with the game world.", 
+            "In an ergonomic chill-working space, the participants join in a game of exponential accumulation of ressources via their smartphones. Activated by their efforts on sport machines, they increase the speed, the growth and their gains.", 
+            "Whoever works the hardest goes the highest!", 
+            "But beware of not losing it all if the battery of your phone runs out!",
+            "There won't be actors. So the challenge is to spark theatricality simply by playing",
+            "and to encourage fruitful conversationnal dynamics",
+            "WE ARE FULL OF HOPE",
+            "Very good so. <i>Travail!</i> is part of my <b>work forecast</b>. It’s linked to <b>technology</b> and <b>technique</b> obviously, to the <b>action verbs</b>, and to <b>design</b> a lot."
+                                    
+        ],            
+        "related":["forecast","design","technology","technique","verbs"],
+        "waiting":[200,150,1000,200,200,1000,100,100,100,100], 
+        "transverse":false
+    },
+
+    {   
+        "key":"deep",
+        "images":[],
+        "texts":[
+            "<i>deep beliefs</i> is the title of my next artistic research starting in 2024, (prolonging the previous one <b>.ext</b>)", 
+            "Where I am entering now is via a different natural landscape and another human practice attached to that environment.", 
+            " u n d e r w a t e r  &&& free-diving (a.k.a <br> a p n e a)",
+            "The exact same socio-historical + symbolico-aesthetical bla-bla as developed in <b>.ext</b> applies here too, so I’ll skip that part and jump to the more juicy one", 
+            "What I find exciting is that free-diving mobilizes the psyche with: to believe.",
+            "Whether it is about achieving outstanding performances in depth or in duration,", 
+            "or about managing the paranoia inherent to going under the water: you are with what you believe.",
+            "bam!",
+            "How I got the first spark of interest was by disapproving of my utter fear of dark waters. (Not <i>limnophobia</i> yet, but in that direction).", 
+            "So, it got me ex-tre-me-ly curious.",
+            "For now, I’ll let you read further on my <b>research methodology</b> and/or my <b>action verbs</b>."
+        ],            
+        "related":["forecast","nature","methodology","verbs"],
+        "waiting":[200,150,1000,200,200,1000,100,100,100,100,100], 
+        "transverse":false
+    },
+
+    {   
+        "key":"vanilla",
+        "images":[],
+        "texts":[
+             "<i>Vanilla</i> is a very immature seed for a potential future project. (in the wake of <b>Silicon Mountain</b> and the sheer joy I have with <b>design</b>.", 
+            "Again an App for smartphones, it will elaborate on the infinite scroll applied to the image of the human body.", 
+            "Imagine an infinite panorama of flesh, skin and hairs, shot in intimacy. It takes you to different body parts as you scroll down, parts you don’t always recognize: is it a neck or the soft flesh of a knee?", 
+            "It brushes more erogenous parts,", 
+            "and as you go down and down and down,", 
+            "the App triggers sexual arousal in you.",
+            "... think nudes, think Grindr, think Only-Fans… Think big money, think <b>internet</b>, <b>technology</b> and <b>entrepreneur</b>!",
+            "first step : make the first Vanilla <i>bodyscape</i> and check if it is soft and sweet like ice-cream.",
+            "Or plain gross disgusting porn."
+
+        ],            
+        "related":["forecast","technology","internet","methodology","design","verbs"],
+        "waiting":[200,150,1000,200,200,1000,100,100,100,100,100], 
+        "transverse":false
+    },
+
 
     {   
         "key":"work",
